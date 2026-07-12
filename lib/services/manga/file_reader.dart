4cc34a21cd153f7cf45ed284b3a95e2c5ad62a3c
Jason A. Januar <jason.kwee98@gmail.com>
2026-07-11 20:21:30 -0700

    FIX selectZip() failing to read zip files as bytes
    also added comments for clarity

--- lib/services/manga/file_reader.dart
+++ lib/services/manga/file_reader.dart
@@ -1,3 +1,4 @@
+import 'dart:io';
 import 'dart:typed_data';
 import 'package:file_picker/file_picker.dart';
 
@@ -10,13 +11,21 @@ class FileReader {
   ///
   /// Returns `null` if no file is selected by the user.
   Future<Uint8List?> selectZip() async {
+    // This opens the file picker dialog for users (limited to zip and cbz)
     FilePickerResult? result = await FilePicker.pickFiles(
       type: FileType.custom,
       allowedExtensions: ["zip", "cbz"]
     );
 
+    // If the user doesn't pick a file (e.g. click cancel), it'll return null
     if(result == null) return null;
 
-    return result.files.single.bytes;
+    // Get file path then use IO to read the file bytes
+    String? path = result.files.single.path;
+    if (path == null) return null;
+    return File(path).readAsBytes();
+
+    // We get the bytes using path instead of result.files.single.bytes because
+    // that one returns null (likely because zip files are too large in size)
   }
 }
