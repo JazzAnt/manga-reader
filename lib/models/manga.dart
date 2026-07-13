ecef2682f0fc49a28fd6f1fcc981060aec2da6b1
Jason A. Januar <jason.kwee98@gmail.com>
2026-07-13 01:24:17 -0700

    add documentation to models

--- lib/models/manga.dart
+++ lib/models/manga.dart
@@ -1,11 +1,15 @@
 import './manga_page.dart';
 
+/// Represents a manga book.
+/// Main purpose is to carry a list of [MangaPage].
 class Manga {
   final String _title;
+  /// The title of the manga.
+  String get title => _title;
+
   final List<MangaPage> _pages;
+  /// A list of [MangaPage] which represents the pages of the manga.
+  List<MangaPage> get pages => _pages;
 
   Manga(this._title, this._pages);
-
-  String get title => _title;
-  List<MangaPage> get pages => _pages;
 }
--- lib/models/manga_page.dart
+++ lib/models/manga_page.dart
@@ -1,13 +1,21 @@
 import 'dart:typed_data';
 
+/// Represents an image file of a single page of a manga.
 class MangaPage {
   final int _index;
+  /// The index of the page. This would normally represent which page it is
+  /// in a `Manga`'s list of pages.
+  ///
+  /// NOTE: Starts at index 0
+  int get index => _index;
+
   final String _filename;
+  /// The filename of the image file this represents (extensions included).
+  String get filename => _filename;
+
   final Uint8List _imageBytes;
+  /// The image data represented as bytes.
+  Uint8List get imageBytes => _imageBytes;
 
   MangaPage(this._index, this._filename, this._imageBytes);
-
-  int get index => _index;
-  String get filename => _filename;
-  Uint8List get imageBytes => _imageBytes;
 }
