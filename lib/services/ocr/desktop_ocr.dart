a79fbe0eedf765fdd9c8db965b317b29337f407d
Jason A. Januar <jason.kwee98@gmail.com>
2026-08-02 23:05:53 -0700

    add android emulator URL for recognizeText

--- lib/services/ocr/desktop_ocr.dart
+++ lib/services/ocr/desktop_ocr.dart
@@ -4,12 +4,20 @@ import 'dart:typed_data';
 import 'package:manga_reader/models/ocr_result.dart';
 import 'package:manga_reader/services/ocr/ocr_service.dart';
 import 'package:http/http.dart' as http;
+import 'package:manga_reader/services/platform/platform_service.dart';
 
 class DesktopOcr implements OcrService {
   // Currently returns dummy text
   @override
   Future<OcrResult> recognizeText(Uint8List imageBytes) async {
-    Uri url = Uri.parse("http://127.0.0.1:8000/ocr");
+    late final Uri url;
+
+    // TODO: this is for android emulator, change implementation for build
+    if (isOnMobile){
+      url = Uri.parse("http://10.0.2.2:8000/ocr");
+    } else {
+      url = Uri.parse("http://127.0.0.1:8000/ocr");
+    }
 
     var request = http.MultipartRequest(
       "POST",
