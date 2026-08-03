72e8b77207b39793f665914e0f354f8a15f56581
Jason A. Januar <jason.kwee98@gmail.com>
2026-08-03 01:18:44 -0700

    add /health api to mangaOCR server

--- python_server/main.py
+++ python_server/main.py
@@ -1,16 +1,23 @@
-from fastapi import FastAPI, File, UploadFile
-from services.ocr_service import recognize_text
+from fastapi import FastAPI, File, UploadFile, HTTPException
+from services.ocr_service import recognize_text, ocr_is_ready
 
 app = FastAPI()
 
-@app.get("/")
-def root():
-    return {"message": "Hello World"}
+@app.get("/health")
+def health():
+    if not ocr_is_ready():
+        raise HTTPException(status_code=503, detail="OCR model not ready")
+
+    return {"ok": True}
 
 @app.post("/ocr")
-async def ocr(
-        image: UploadFile = File(...),
-):
+async def ocr(image: UploadFile = File(...)):
+    if not ocr_is_ready():
+        raise HTTPException(status_code=503, detail="OCR model not ready")
+
+    # TODO: handle other types of exceptions such as uploading non-image file
+    # TODO: or OCR failure, or file too large
+
     image_bytes = await image.read()
     recognized_text = recognize_text(image_bytes)
 
