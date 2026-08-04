bf3825b2372ce0ffb3799233568a4b36adc02ce4
Jason A. Januar <jason.kwee98@gmail.com>
2026-08-04 03:18:41 -0700

    improve OcrScreen UI

--- lib/screens/ocr_screen.dart
+++ lib/screens/ocr_screen.dart
@@ -1,5 +1,6 @@
 import 'dart:async';
 
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:manga_reader/models/dictionary_entry.dart';
@@ -16,60 +17,94 @@ class OcrScreen extends ConsumerStatefulWidget {
 
 class _OcrScreenState extends ConsumerState<OcrScreen> {
   String _placeholderText = "Awaiting Selection";
+  bool _lookingUp = false;
   List<DictionaryEntry> _entries = [];
   Timer? _selectionDelay;
   @override
   Widget build(BuildContext context) {
-    return Column(
-      children: [
-        SelectionArea(
-          child: Text( //TODO: replace with actual OCR text
-            "私はばかです。でもあなたよりもっとばかすぎる。お前はせかいいちばかにんげん。ばかばかばか",
-            style: TextStyle(fontSize: 30, fontWeight: FontWeight.bold),
-          ),
-          onSelectionChanged: (selection) {
-            _selectionDelay?.cancel();
+    return Padding(
+      padding: .all(10),
+      child: Column(
+        children: [
+          Container(
+            decoration: BoxDecoration(
+              color: Colors.lightBlueAccent,
+              border: .all(color: CupertinoColors.activeBlue, width: 2),
+              borderRadius: .circular(6),
+            ),
+            child: SelectionArea(
+              child: Text(
+                //TODO: replace with actual OCR text
+                "私はばかです。でもあなたよりもっとばかすぎる。お前はせかいいちばかにんげん。ばかばかばか",
+                style: TextStyle(fontSize: 30, fontWeight: FontWeight.bold),
+              ),
+              onSelectionChanged: (selection) {
+                _selectionDelay?.cancel();
 
-            final keyword = selection?.plainText;
-            if (keyword == null || keyword.isEmpty) {
-              setState(() {
-                _entries = [];
-                _placeholderText = "Awaiting Selection";
-              });
-              return;
-            }
+                final keyword = selection?.plainText;
+                if (keyword == null || keyword.isEmpty) {
+                  setState(() {
+                    _entries = [];
+                    _placeholderText = "Awaiting Selection";
+                    _lookingUp = false;
+                  });
+                  return;
+                }
 
-            setState(() {
-              _entries = []; // To remove previous entries.
-              _placeholderText = "Looking up $keyword";
-            });
-
-            // Selection finalized if selection has stopped for more than 350ms
-            _selectionDelay = Timer(
-              const Duration(milliseconds: 350),
-              () async {
-                print("Starting entry lookup");
-                final entries = await DictionaryService().lookup(keyword);
-                print("Ending entry lookup");
-                if (!mounted) return;
                 setState(() {
-                  _entries = entries;
+                  _entries = []; // To remove previous entries.
+                  _placeholderText = "Looking up 「$keyword」";
+                  _lookingUp = true;
                 });
-              },
-            );
-          },
-        ),
-        Expanded(
-          child: _entries.isNotEmpty
-              ? ListView.builder(
-                  itemCount: _entries.length,
-                  itemBuilder: (context, index) {
-                    return DictionaryDisplay(dictionaryEntry: _entries[index]);
+
+                // Selection finalized if selection has stopped for more than 350ms
+                _selectionDelay = Timer(
+                  const Duration(milliseconds: 350),
+                  () async {
+                    final entries = await DictionaryService().lookup(keyword);
+                    if (!mounted) return;
+
+                    setState(() {
+                      _entries = entries;
+                      _placeholderText = _entries.isEmpty
+                          ? "Cannot find definition of 「$keyword」"
+                          : "Awaiting Selection";
+                      _lookingUp = false;
+                    });
                   },
-                )
-              : Center(child: Text(_placeholderText)),
-        ),
-      ],
+                );
+              },
+            ),
+          ),
+          Expanded(
+            child: _entries.isNotEmpty
+                ? ListView.builder(
+                    itemCount: _entries.length,
+                    itemBuilder: (context, index) {
+                      return DictionaryDisplay(
+                        dictionaryEntry: _entries[index],
+                      );
+                    },
+                  )
+                : Column(
+                    mainAxisAlignment: .center,
+                    children: [
+                      Text(
+                        _placeholderText,
+                        style: TextStyle(fontWeight: .bold, fontSize: 30),
+                      ),
+                      SizedBox(height: 11),
+                      _lookingUp
+                          ? CircularProgressIndicator(
+                              strokeWidth: 10,
+                              color: Colors.green,
+                            )
+                          : SizedBox(),
+                    ],
+                  ),
+          ),
+        ],
+      ),
     );
   }
 }
